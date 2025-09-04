845afeb8f31370ec5c639da4adf3416022930867
Michael Maher <michael.maher.nagi@gmail.com>
2025-09-05 02:22:28 +0300

    Update dependencies and Gradle versions
    Upgraded Flutter, Dart, and various package dependencies to latest versions in pubspec.yaml and pubspec.lock. Updated Android Gradle plugin and Kotlin versions, set jvmTarget for Kotlin, and bumped Gradle wrapper to 8.7. Added new plugins and updated plugin registration for macOS. These changes improve compatibility and ensure the project uses current tooling.

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,14 @@ import Foundation
 
 import geolocator_apple
 import location
+import package_info_plus
+import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
