6f1b340974bc4738307486f4fd2ee26f9de5310c
Michael Maher <michael.maher.nagi@gmail.com>
2025-09-05 05:11:17 +0300

    Remove flutter_map_cancellable_tile_provider dependency
    Eliminates the flutter_map_cancellable_tile_provider package from dependencies and updates location_picker.dart to use NetworkTileProvider instead of CancellableNetworkTileProvider. This simplifies tile provider usage and removes unused transitive dependencies.

--- lib/src/location_picker.dart
+++ lib/src/location_picker.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:intl/intl.dart' as intl;
 import 'package:latlong2/latlong.dart';
@@ -1275,7 +1274,7 @@ class _FlutterLocationPickerState extends State<FlutterLocationPicker>
             subdomains: const ['a', 'b', 'c'],
             userAgentPackageName: widget.userAgent,
             tileProvider: _mapConfig.customTileProvider ??
-                CancellableNetworkTileProvider(),
+                NetworkTileProvider(),
             additionalOptions: _mapConfig.tileRequestHeaders ?? {},
           ),
           if (widget.showCurrentLocationPointer) _buildCurrentLocation(),
